abf62bd9026ca1715b83dc6b09795ab5d19a71c3
3t14 <michiru@r4hs.com>
2016-12-03 04:46:56 +0900

    引数をコンソールに出力する文の追加

--- Program.cs
+++ Program.cs
@@ -16,6 +16,12 @@ namespace xr1_1
 			// 引数の数を出力
 			Console.WriteLine("引数の数：{0}", args.Length);
 
+			// 各引数を出力
+			for (int i = 0; i < args.Length; i++)
+			{
+				Console.WriteLine("Arg[{0}] = [{1}]", i, args[i]);
+			}
+
 			// デバッグモードでウインドウを開いた状態にし続ける
 			Console.WriteLine("何かキーを入力すると終了します。");
 			// 入力待ち状態にする
