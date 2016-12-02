5cc2fb4aa0f78b2fcc434d36920cbb683793a1a2
Michiru Tanaka <michiru@r4hs.com>
2016-12-03 02:59:24 +0900

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿/**
+ 複数行のコメントを
+ このように記述することもできます。
+ */ 
+using System;
+
+namespace xr1_1
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			// コンソールに初めてのメッセージを出力
+			Console.WriteLine("Hello World!");
+			// デバッグモードでウインドウを開いた状態にし続ける
+			Console.WriteLine("何かキーを入力すると終了します。");
+			// 入力待ち状態にする
+			Console.ReadKey();
+		}
+	}
+}
+
